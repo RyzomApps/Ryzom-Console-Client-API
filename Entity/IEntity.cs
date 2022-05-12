19394ec46c3ba33efac1d12f94faf312aac1dd98
bierdosenhalter <529651+bierdosenhalter@users.noreply.github.com>
2022-05-12 10:34:22 +0200

    merge

--- Entity/IEntity.cs
+++ Entity/IEntity.cs
@@ -1,12 +1,4 @@
-﻿///////////////////////////////////////////////////////////////////
-// This file contains modified code from 'Ryzom - MMORPG Framework'
-// http://dev.ryzom.com/projects/ryzom/
-// which is released under GNU Affero General Public License.
-// http://www.gnu.org/licenses/
-// Copyright 2010 Winch Gate Property Limited
-///////////////////////////////////////////////////////////////////
-
-using System.Numerics;
+﻿using System.Numerics;
 
 namespace API.Entity
 {
