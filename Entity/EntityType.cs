d2042250fc2cca9da9db54c55eba3fc7ef33bdb3
bierdosenhalter <529651+bierdosenhalter@users.noreply.github.com>
2022-05-12 08:41:06 +0200

    More methods for entity API

---
+++ Entity/EntityType.cs
@@ -0,0 +1,24 @@
+﻿///////////////////////////////////////////////////////////////////
+// This file contains modified code from 'Ryzom - MMORPG Framework'
+// http://dev.ryzom.com/projects/ryzom/
+// which is released under GNU Affero General Public License.
+// http://www.gnu.org/licenses/
+// Copyright 2010 Winch Gate Property Limited
+///////////////////////////////////////////////////////////////////
+
+namespace API.Entity
+{
+    /// <summary>
+    /// Primitive type
+    /// </summary>
+    public enum EntityType
+    {
+        User = 0,
+        Player,
+        NPC,
+        Fauna,
+        Entity,
+        ForageSource,
+        TypeCount
+    }
+}
