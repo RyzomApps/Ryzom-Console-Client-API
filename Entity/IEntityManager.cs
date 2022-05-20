06028637a590e99c44ffdbda2e773c0dfb14d110
bierdosenhalter <529651+bierdosenhalter@users.noreply.github.com>
2022-05-20 10:54:12 +0200

    added getentitybyname to the api

--- Entity/IEntityManager.cs
+++ Entity/IEntityManager.cs
@@ -13,5 +13,13 @@ namespace API.Entity
         /// Contain all 256 entities around the player
         /// </summary>
         List<IEntity> GetApiEntities();
+
+        /// <summary>
+        /// Get an entity by name. Returns NULL if the entity is not found.
+        /// </summary>
+        /// <param name="name">of the entity to find</param>  
+        /// <param name="caseSensitive">type of test to perform</param>  
+        /// <param name="complete">if true, the name must match the full name of the entity</param>
+        public IEntity GetEntityByName(string name, bool caseSensitive, bool complete);
     }
 }
