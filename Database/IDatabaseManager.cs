c1867bf2e26f5ac6a3abdc2610d2a769bb3384bc
bierdosenhalter <529651+bierdosenhalter@users.noreply.github.com>
2022-06-01 08:51:58 +0200

    added database reset bank to api

--- Database/IDatabaseManager.cs
+++ Database/IDatabaseManager.cs
@@ -8,5 +8,12 @@
         /// <param name="name">is the name of the property</param>
         /// <returns>the value of the property</returns>
         long GetProp(string name);
+
+        /// <summary>
+        /// Resets the specified bank. All leaf values are set to 0.
+        /// </summary>
+        /// <param name="gc">GameCycle (no idea what it is exactly, probably some time value)</param>
+        /// <param name="bank">The banks we want to reset</param>
+        void ResetBank(in uint gc, in uint bank);
     }
 }
